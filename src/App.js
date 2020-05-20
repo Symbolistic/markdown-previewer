ab7e0eea23326a70b4c71196945894e97d32457c
Symbol <Symbolistic@live.com>
2020-05-20 02:07:55 -0400

    First commit to deploy Markdown Previewer, convert markdown into HTML!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MarkdownPreviewer from './MarkdownPreviewer'
+import './style.css'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="grid-container">
+      <Header />
+      <MarkdownPreviewer />
+      <Footer />
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+function Footer () {
+    return (
+        <footer><p>Copyright &copy; 2020</p></footer>
+    )
+}
+
+export default Footer
